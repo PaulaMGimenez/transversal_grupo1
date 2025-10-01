9d20a964e85ee0af45dbe9ecfc729073e52b6807
jonatan <david.joni2401@gmail.com>
2025-09-30 23:42:46 -0300

    Percistencia
    Cree alumnoData ya listo y terminado

--- Universidad_Gp1/src/Percistencia/alumnoData.java
+++ Universidad_Gp1/src/Percistencia/alumnoData.java
@@ -7,12 +7,14 @@ package Percistencia;
 
 import Modelo.alumno;
 import Modelo.myConexion;
+import com.mysql.jdbc.Statement;
 import com.sun.istack.internal.logging.Logger;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -30,24 +32,25 @@ public class alumnoData {
         String query = "INSERT INTO alumno(idAlumno, dni, apellido, nombre, fechaNacimiento, estado) VALUES(?,?,?,?,?,?)";
 
         try {
-            PreparedStatement ps = conn.prepareStatement(query, Statemet)
-            ps.setInt(1, a.getIdAlumno());
-            ps.setInt(2, a.getDni());
-            ps.setString(3, a.getApellido());
-            ps.setString(4, a.getNombre());
-            ps.setDate(5, (Date) a.getFechaNacimiento());
-            ps.setInt(6, a.getEstado());
-            ps.executeUpdate();
-            ResultSet rs = ps.getGeneratedKeys();
-            if (rs.next()) {
-                a.setIdAlumno(rs.getInt(1));
-            } else {
-                System.out.println("No se pudo obtener ID");
+            try (PreparedStatement ps = conn.prepareStatement(query,Statement.RETURN_GENERATED_KEYS)) {
+                ps.setInt(1, a.getIdAlumno());
+                ps.setInt(2, a.getDni());
+                ps.setString(3, a.getApellido());
+                ps.setString(4, a.getNombre());
+                ps.setDate(5, (Date) a.getFechaNacimiento());
+                ps.setInt(6, a.getEstado());
+                ps.executeUpdate();
+                ResultSet rs = ps.getGeneratedKeys();
+                if (rs.next()) {
+                    a.setIdAlumno(rs.getInt(1));
+                    JOptionPane.showMessageDialog(null, "Alumno Guardado Exitosamente");
+                } else {
+                    System.out.println("No se pudo obtener ID");
+                }
             }
-            ps.close();
             System.out.println("Guardado");
         } catch (SQLException ex) {
-            Logger.getLogger(alumnoData.class.getName())
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabala alumno");
         }
     }
 }
